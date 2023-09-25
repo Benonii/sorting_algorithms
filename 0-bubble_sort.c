6f1616bd0ab33d53057bfa4660bdf1f284dc25b5
root <besckinder@gmail.com>
2023-09-25 15:54:24 -0700

    checker fix attempt 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,27 +10,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp, swap;
+	size_t i, j, temp;
 
 	if (array == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		swap = 0;
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swap = 1;
+				print_array(array, size);
 			}
 		}
-		if (swap == 1)
-			print_array(array, size);
-		else
-			break;
 	}
 }
